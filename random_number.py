f7b510f54f4e4f7bfe9e79ee97fd9e73498a3db7
Marshall Demars <marshall.demars@mths.ca>
2022-10-18 14:24:49 +0000

    added period

--- random_number.py
+++ random_number.py
@@ -19,7 +19,7 @@ def main():
     if user_number == random_number:
         print("\nYou guessed correctly!")
     else:
-        print("\nYou guessed incorrectly, the number was {}".format(random_number))
+        print("\nYou guessed incorrectly, the number was {}.".format(random_number))
 
     print("\nDone.")
 
