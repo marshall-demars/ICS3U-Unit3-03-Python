49567c3885028d24a57055f9523a39ae35539434
Marshall Demars <marshall.demars@mths.ca>
2022-10-18 14:17:26 +0000

    new file

---
+++ random_number.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: October 2022
+# This program uses if statements to guess a random number
+#    with user input
+
+import random
+
+
+def main():
+    # this uses if statements to guess a random number
+
+    # input
+    random_number = random.randint(1, 9)  # a number between 1 and 9
+    user_number = int(input("Enter a number between 1 and 10: "))
+
+    # process and output
+    if user_number == random_number:
+        print("\nYou guessed correctly!")
+    else:
+        print("\nYou guessed incorrectly, the number was {}".format(random_number))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
